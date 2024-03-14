6da3d484e9f80ef5a9f82cda3d10f820f590e8b9
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:50:03 +0530

    Initial Push

---
+++ EstateEaseMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EstateEaseMAUI
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ EstateEaseMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EstateEaseMAUI
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
